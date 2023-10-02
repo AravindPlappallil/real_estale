98a2a41d0f8bd6bc261e9159384592672ddc6c67
AravindPlappallil <athiraaswin@ATHIRAs-MacBook-Air.local>
2023-10-02 12:39:56 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/Sign-in' element={<Signin />} />
+      <Route path='/Sign-up' element={<SignUp />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Profile' element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
