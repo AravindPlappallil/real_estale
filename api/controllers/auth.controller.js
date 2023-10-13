9c6595669bafdb43b64807c6fd70aeb34ca570a1
AravindPlappallil <athiraaswin@ATHIRAs-MacBook-Air.local>
2023-10-13 19:44:38 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+import  jwt from 'jsonwebtoken';
+
 // import { errorHandler } from '../utils/error.js';
 export const signup=async(req,res,next)=>{
 
@@ -14,7 +17,20 @@ catch(error){
     next(error);
 // next(errorHandler(550,'error from the function '));
 }
+};
 
-
-
+// sign in
+export const signin= async (req,res,next)=>{
+    const {email, password}= req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'user not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);   //used to compare the bcryptjs. to get hashed password
+        if (!validPassword) return next(errorHandler(401,'Invalid email or password!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET, );
+        const {password:pass, ...rest} = validUser._doc;  // to hide the password for the safety
+        res.cookie('acces_token', token, {httpOnly:true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    } 
 }
